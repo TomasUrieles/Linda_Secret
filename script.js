fc04b8e021a6500620d50a84a4ffdfa89b95dc3e
Tomas Urieles <tomasurieles31@gmail.com>
2026-04-02 13:55:17 -0500

    js intento

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const intro = document.getElementById("intro");
-const universe = document.getElementById("universe");
-const letterScene = document.getElementById("letterScene");
 
-intro.addEventListener("click", () => {
-    intro.classList.add("hidden");
-    setTimeout(()=> universe.classList.remove("hidden"),1500);
-});
 
-document.getElementById("toLetter").addEventListener("click", ()=>{
-    universe.classList.add("hidden");
-    setTimeout(()=> letterScene.classList.remove("hidden"),1500);
+document.addEventListener("DOMContentLoaded", function () {
+
+    const intro = document.getElementById("intro");
+    const universe = document.getElementById("universe");
+    const letterScene = document.getElementById("letterScene");
+    const toLetterBtn = document.getElementById("toLetter");
+
+    intro.addEventListener("click", () => {
+        intro.classList.add("hidden");
+
+        setTimeout(() => {
+            universe.classList.remove("hidden");
+        }, 1500);
+    });
+
+    toLetterBtn.addEventListener("click", () => {
+        universe.classList.add("hidden");
+
+        setTimeout(() => {
+            letterScene.classList.remove("hidden");
+        }, 1500);
+    });
+
 });
