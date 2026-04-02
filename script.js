ccd97dfa9688d4f4b5524f738be468bed648fa54
Tomas Urieles <tomasurieles31@gmail.com>
2026-04-02 13:49:36 -0500

    Arreglos y primera carta

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const startBtn = document.getElementById("startBtn");
 const intro = document.getElementById("intro");
 const universe = document.getElementById("universe");
+const letterScene = document.getElementById("letterScene");
 
-startBtn.addEventListener("click", () => {
+intro.addEventListener("click", () => {
     intro.classList.add("hidden");
+    setTimeout(()=> universe.classList.remove("hidden"),1500);
+});
 
-    setTimeout(() => {
-        universe.classList.remove("hidden");
-    }, 1500);
+document.getElementById("toLetter").addEventListener("click", ()=>{
+    universe.classList.add("hidden");
+    setTimeout(()=> letterScene.classList.remove("hidden"),1500);
 });
